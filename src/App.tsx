f3e7ab0360095107ddbf4a6eacd8d9852958f6c9
Mahdi Pourtalebi <mahdi.pourtalebi@gmail.com>
2023-07-16 16:08:35 +0330

    configured vite to use tsconfig for import, basic navbar done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css'
-
+import Nav from 'components/Nav'
 function App() {
-  return <></>
+  return (
+    <>
+      <Nav items={['Home', 'Categories', 'Shop', 'About', 'Contact']} />
+    </>
+  )
 }
 
 export default App
