002960581a348e4c11c49e773f3f994dcf6c99d8
Mahdi Pourtalebi <mahdi.pourtalebi@gmail.com>
2023-07-22 10:54:31 +0330

    removed project 1 and 3 from the branch

--- project01/src/components/Card/CardContent/CardContent.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode } from 'react'
-
-function CardContent({children}:{children:ReactNode}):React.ReactNode{
-  return (
-    <div className='text-gray text-sm'>{children}</div>
-  )
-}
-
-export default CardContent
