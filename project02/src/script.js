0fabd26ed468467bb4a65511525752d58a92f6aa
Mahdi Pourtalebi <mahdi.pourtalebi@gmail.com>
2023-07-22 16:56:21 +0330

    added touch scroll (not smooth)

--- project02/src/script.js
+++ project02/src/script.js
@@ -298,3 +298,46 @@ const cardContainer5 = document.getElementById('list-container5')
   text.id = null
   cardContainer5.appendChild(clone)
 })
+
+const ele = document.getElementById('sliderContainer')
+ele.scrollTop = 100
+ele.scrollLeft = 150
+
+let pos = { top: 0, left: 0, x: 0, y: 0 }
+
+const mouseMoveHandler = function (e) {
+  // How far the mouse has been moved
+  const dx = e.clientX - pos.x
+  const dy = e.clientY - pos.y
+
+  // Scroll the element
+  ele.scrollTop = pos.top - dy
+  ele.scrollLeft = pos.left - dx
+}
+const mouseUpHandler = () => {
+  document.removeEventListener('mousemove', mouseMoveHandler)
+  document.removeEventListener('mouseup', mouseUpHandler)
+
+  ele.style.scrollBehavior = 'smooth'
+  ele.style.scrollSnapType = 'x mandatory'
+  ele.style.cursor = 'grab'
+}
+
+const mouseDownHandler = e => {
+  pos = {
+    // The current scroll
+    left: ele.scrollLeft,
+    top: ele.scrollTop,
+    // Get the current mouse position
+    x: e.clientX,
+    y: e.clientY,
+  }
+
+  document.addEventListener('mousemove', mouseMoveHandler)
+  document.addEventListener('mouseup', mouseUpHandler)
+  ele.style.scrollSnapType = 'none'
+  ele.style.scrollBehavior = 'unset'
+  ele.style.cursor = 'grabbing'
+}
+
+document.addEventListener('mousedown', mouseDownHandler)
