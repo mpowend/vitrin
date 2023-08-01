35ac81c55c0d0b656fd74c654eb07b755df1e84b
NimaSaei379 <saeinima@yahoo.com>
2023-08-01 15:28:13 +0330

    clean up some unNecessary components

--- src/components/Slider/SliderItem.tsx
+++
@@ -1,9 +0,0 @@
-function SliderItem({ src }: { src: string }) {
-  return (
-    <div className="sliderItem">
-      <img draggable="false" src={src} />
-    </div>
-  )
-}
-
-export default SliderItem
