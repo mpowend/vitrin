bd2203201feabbbca81f52d70631131072278405
Mahdi Pourtalebi <mahdi.pourtalebi@gmail.com>
2023-08-05 18:15:56 +0330

    added docker

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    host: true,
+  },
 
   resolve: {
     alias: {
